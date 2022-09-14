9aaa40b6d79d04b21b76732819c76b6835822271
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-14 07:58:20 -0700

    issue fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,20 +10,43 @@ void jack_bauer(void)
 
 	for (i = 0; i < 3; i++)
 	{
-		for (j = 0; j < 4; j++)
+		if (i == 2)
+		{
+			for (j = 0; j < 4; j++)
+			{
+				for (k = 0; k < 6; k++)
+				{
+					for (l = 0; l < 10; l++)
+					{
+						_putchar('0' + i);
+						_putchar('0' + j);
+                                                _putchar(':');
+                                                _putchar('0' + k);
+                                                _putchar('0' + l);
+                                                _putchar('\n');
+
+					}
+				}
+			}
+		}
+		else{
+
+		for (j = 0; j < 10; j++)
 		{
 			for (k = 0; k < 6; k++)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					_putchar('0' + i);
-					_putchar('0' + j);
-					_putchar(':');
-					_putchar('0' + k);
-					_putchar('0' + l);
-					_putchar('\n');
+						_putchar('0' + i);
+						_putchar('0' + j);
+						_putchar(':');
+						_putchar('0' + k);
+						_putchar('0' + l);
+						_putchar('\n');
+					
 				}
 			}
+		}
 		}
 	}
 }
