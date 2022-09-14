99f59820a1f269008064c82d3c3325c48c54b915
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-14 03:15:36 -0700

    task 6 done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _abs - computes the absoulute value of an integer
+ *
+ * @x: the number to be computed
+ *
+ * Return: absoulte value of an integer
+ */
+int _abs(int x)
+{
+	int y = -1;
+
+	if (x < 0)
+	{
+		return (y * x);
+	}
+	return (x);
+}
