6191199c424d5670cc6bca53d333ec1d0e877de2
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-15 14:32:22 -0700

    task 10 done

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_triangle - prints the character in tirangle shape
+ * @size: contains integer for the size of the triangle
+ * Return: none
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = size; i > 0; i--)
+	{
+		for (j = 0; j < i; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = size; k >= i; k--)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
