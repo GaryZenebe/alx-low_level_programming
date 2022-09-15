3fcbe1afab20eb30ad8a2f8d23061027e8c63262
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-15 12:12:50 -0700

    task 7 done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_diagonal - print character
+ *
+ * @n: number of times \ to be printed
+ *
+ * Return: nothing
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
