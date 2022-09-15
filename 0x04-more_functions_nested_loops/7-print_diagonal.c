f7f8f634cf52b4e49283818c0c4648bccabbb424
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-15 13:17:58 -0700

    if condition added

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,17 +10,24 @@ void print_diagonal(int n)
 {
 	int i, j;
 
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar('\\');
 		_putchar('\n');
-		if (i == n - 1)
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
 		{
-			break;
-		}
-		for (j = 0; j < i + 1; j++)
-		{
-			_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+			if (i == n - 1)
+			{
+				break;
+			}
+			for (j = 0; j < i + 1; j++)
+			{
+				_putchar(' ');
+			}
 		}
 	}
 }
