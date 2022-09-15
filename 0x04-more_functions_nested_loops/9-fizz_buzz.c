eefd345d194faf13579eecb5cb3da83de5116759
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-15 14:01:37 -0700

    issue fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,18 @@ int main(void)
 	{
 		if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			printf("%c", ' ');
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			printf("%c", ' ');
 		}
 		else if (i % 5 == 0 && i % 3 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			printf("%c", ' ');
 		}
 		else
 		{
