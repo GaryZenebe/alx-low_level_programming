a1e86de587de6dbc68af9905b1b1d5bd11142e5e
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-16 12:01:28 -0700

    issue fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,11 +7,33 @@ void more_numbers(void)
 {
 	int i = 0;
 	int j = 0;
+	int k, l;
+
 
 	for (j = 0; j < 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
-		_putchar('0' + i);
+		{
+			if (i >= 10)
+			{
+				int a = 0;
+
+				k = i / 10;
+
+				l = i % 10;
+
+				while (a < 2)
+				{
+					_putchar('0' + k);
+					k = l;
+					a++;
+				}
+			}
+			else
+			{
+				_putchar('0' + i);
+			}
+		}
 		_putchar('\n');
 	}
 }
