6155e3f40fd24fc79e727e5ffce8863cf53a0992
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-17 03:07:29 -0700

    task 4 done

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry of a function
+ *
+ * Return: nothing
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
