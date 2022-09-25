99a2ca6523a19519cbbdf467684f92c3e1ea8765
GaryZenebe <Gedion.Zenebe@gmail.com>
2022-09-25 05:27:16 -0700

    folder containing projects on pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * reset_to_98 - updates the value it points to
+ * @n: pointer containing the address of a variable
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Project files for 0x05. C - Pointers, arrays and strings
